dc5644c61a6bb4e56c78fd6968553b9fc1182895
Suresh Rajan <sureshrajan.perumal@cesltd.com>
2020-02-05 20:39:12 +0530

    changed commit

--- GitPractice/src/Git/Packageclass.java
+++ GitPractice/src/Git/Packageclass.java
@@ -3,7 +3,7 @@ package Git;
 public class Packageclass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World Message by  Suresh Rajan");
+		System.out.println("Hello World Message by  Suresh Rajan Perumal");
 
 	}
 
